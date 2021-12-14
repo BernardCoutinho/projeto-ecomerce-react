299a9f80d2fce64d0ea9eb3ba9c742fff858f16e
BernardCoutinho <80927497+BernardCoutinho@users.noreply.github.com>
2021-12-14 19:21:34 -0300

    carrinho

--- src/pages/DetalheProduto/index.jsx
+++ src/pages/DetalheProduto/index.jsx
@@ -1,71 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image } from "react-native";
-import axios from "axios";
+import { Text, Image } from "react-native";
 import { styles } from "./styles";
-<<<<<<< HEAD
-import Api from "../../service/api"
-import { Button } from "react-native-elements";
-import  {createItem} from  '../../repository/productRepository';
-
-
-  export default function DetalheProduto({route}) {
-
-    const [prod, setProd] = useState([]);
-    const [quant, setQuant] = useState(null);
-    const getDatabyId = async () => {
-      await  Api.get(`/produtos/${id}`).then((response) =>{
-        setProd(response.data)
-        console.log(response.data)
-      } ) 
-    };
-
-      useEffect(() =>{
-        getDatabyId();
-      }, [])
-
-    const {id} = route.params
-    console.log("id",)
-
-    
-
-   const onHandleClick = async () => { 
-      await createItem(prod.name, prod.descricao, prod.vlUnitario, prod.vlUnitario, quant)
-    
-   }
-    
-    return (
-      <View style={styles.container1}>
-        <View>
-           <View>
-             <Image style={{width: 200, height: 200  }} source ={{uri: prod.imagens}} resizeMode="cover"/>
-          </View>
-          <View style={styles.container1}>
-            
-            <Text> nome = {prod.nome} </Text>
-            <Text> valor = {prod.vlUnitario} </Text>
-            <View>
-            <TextInput type={Number}
-            style={styles.inserirQuantidade}
-            placeholder={"Insira seu endereço"}
-            value={quantidade}
-            onChangeText={(value) => {
-              setQuant(value)}}
-            />
-        </View>
-            <Button title="adicionar" onPress={onHandleClick}/>
-          </View>
-        </View>
-      </View>
-   
-=======
-import Api from "../../service/api";
-import { createItem } from "../../repository/productRepository";
+import CustomButton from "../../components/Button";
 import { TouchableOpacity } from "react-native-web";
-import ButtonCar from "../../components/Buttoncar";
-
+import Api from "../../service/api"
+import  {createItem} from  '../../repository/productRepository';
+import { useNavigation } from '@react-navigation/native';
 
 export default function DetalheProduto({ route }) {
   const [prod, setProd] = useState([]);
+  const [quant, setQuant] = useState(null);
   const getDatabyId = async () => {
     await Api.get(`/produtos/${id}`).then((response) => {
       setProd(response.data);
@@ -86,19 +30,25 @@ export default function DetalheProduto({ route }) {
   return (
 
         <TouchableOpacity style={styles.container1}>
+          
         <Image
           style={styles.img}
           source={{ uri: prod.imagens }}
-          resizeMode="cover"
+          resizeMode="center"
           />
-          <View style={styles.textContainer}>
           <Text style={styles.nomeP}>{prod.nome}</Text>
-          <Text style={styles.preco}>R$ {prod.vlUnitario}</Text>
+          <Text>R$ {prod.vlUnitario}</Text>
           <Text> descrição </Text>
           <Text>{prod.descricao}</Text>
-          </View>
-          <ButtonCar/>
+          <TextInput type={Number}
+            style={styles.inserirQuantidade}
+            placeholder={"Insira seu endereço"}
+            value={quantidade}
+            onChangeText={(value) => {
+              setQuant(value)}}
+            />
+          <CustomButton onPress={onHandleClick} title="Adicionar ao Carrinho" />
           </TouchableOpacity>
->>>>>>> 578a4394faa12cff247ff60e775e8226e8f3888c
+
   );
 }
